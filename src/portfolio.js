3cd9def942e2dba96cde52f7c8015fae0d531d66
sandeepr16 <ragsand99@gmail.com>
2024-02-25 11:51:23 -0500

    Second

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,22 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Sandeep Ragampudy",
+  title: "Hi all, I'm Sandeep",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "As a passionate and ambitious Engineer 🛠️, I thrive on understanding system intricacies and building scalable services using diverse technologies and cloud platforms ☁️. Driven by curiosity, I constantly explore software engineering depths, embracing challenges and learning through hands-on experience 👨‍💻 to develop innovative solutions and enhance my skills through cool projects 🚀."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1kUKr_XhO_VahieDDL4cq0v9iXhEU32Y5/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/sandeepr16",
+  linkedin: "https://www.linkedin.com/in/ragampudy-sandeep-b8157614b/",
+  gmail: "sandeep.ragampudy@gmail.com",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
